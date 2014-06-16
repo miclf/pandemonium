9986c24a85c4ae769f835eb81a3ef7b8ce9dc0d3
miclf <michael@estsurinter.net>
2014-06-16 23:47:24 +0200

    [meta] Require-dev way/generators
    Laravel 4 Generators
    https://github.com/JeffreyWay/Laravel-4-Generators
    
    This adds commands to Artisan allowing to quickly generate some kinds of files.
    
    The related service provider needs to be loaded in local environments only. This commit then adds a dedicated section if the local app config file.

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -15,4 +15,22 @@ return array(
 
 	'debug' => true,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
+
+    'providers' => append_config(array(
+
+        // Third-party service providers
+        'Way\Generators\GeneratorsServiceProvider',
+
+    )),
+
 );
