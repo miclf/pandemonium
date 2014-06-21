372efda964f9120f3f5929e1080fa53edb8fc8de
miclf <michael@estsurinter.net>
2014-06-21 23:27:06 +0200

    [config] Add security config file
    Add a configuration file to store and centralize some security settings of the application.
    
    The (currently) only option is the cost factor of the hash algorithm.

---
+++ app/config/security.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cost factor of hash algorithm
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the number of rounds to use when hashing strings.
+    | The time needed to generate the hash will increase in proportion to
+    | this cost factor, making this system scale with processing power.
+    |
+    */
+
+    'hash_cost_factor' => 12,
+
+];
